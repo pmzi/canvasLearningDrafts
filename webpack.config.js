b0d3358b71653a1ea444700c5c56bffeab225c32
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-08-24 21:00:44 +0430

    webpack configs added

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    mode: 'production',
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+        filename: './bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015','react'] }
+            }, {
+                test: /\.sass$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader?-url" }, { loader: "sass-loader" }]
+            }]
+    }
+};
