9b7045984e60a492a873e221e65e9c67e2ca00a7
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-08-24 23:47:03 +0430

    crash for edges of the canvas is now operational

--- src/components/Rect.js
+++ src/components/Rect.js
@@ -1,21 +1,35 @@
 class Rect{
 
-    constructor(context, property = {
+    constructor(app, property = {
         x:0,
         y:0,
         width:50,
-        height:50
+        height:50,
+        speedX: 10,
+        speedY: 10
     }){
 
-        this.context = context;
+        this.context = app.context;
+
+        this.app = app;
 
         this.property = property;
         
     }
 
     __move(){
-        this.property.x++;
-        this.property.y++;
+        this.__checkCrash();
+        this.property.x+=this.property.speedX;
+        this.property.y+=this.property.speedY;
+    }
+
+    __checkCrash(){
+        if(this.property.x + this.property.width >= this.app.width || this.property.x < 0){
+            this.property.speedX*=-1;
+        }
+        if(this.property.y + this.property.height >= this.app.height || this.property.y < 0){
+            this.property.speedY*=-1;
+        }   
     }
 
     render(){
